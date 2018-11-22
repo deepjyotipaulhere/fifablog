117e6fac4c443fe58d03c44933afe7fd43ba88d1
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2018-11-22 21:57:13 +0530

    modified:   pages/index.vue 	new file:   pages/register.vue 	new file:   service.py new

---
+++ pages/register.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="header">
+            <div class="bx--grid">
+                <div class="bx--row">
+                <div class="bx--col-xs-12 " style="padding:2rem 0">
+                    <h5 style="color:#5a6872;font-weight:400;margin:0">Welcome</h5>
+                    <h1 class="page-title" style="margin:0">Blog</h1>
+                </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="bx--grid">
+            <div class="bx--form-item">
+                <input id="text-input-3" type="text" class="bx--text-input" placeholder="Optional placeholder text">
+                <label for="text-input-3" class="bx--label">Text Input label</label>
+            </div>
+            <div class="bx--form-item">
+                <textarea id="text-area-2" class="bx--text-area" rows="4" cols="50" placeholder="Placeholder text."></textarea>
+                <label for="text-area-2" class="bx--label">Text Area label</label>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
